cd30fdecf3152e9d44ce045c0359ee23f5a34151
Rui Liu <rliu@cs.dartmouth.edu>
2018-07-22 16:45:43 -0400

    fake test

--- src/qjobs/generate_rev2_test_qjobs.py
+++ src/qjobs/generate_rev2_test_qjobs.py
@@ -1,15 +1,17 @@
+#!/usr/bin/env python3
+
+sample='''
 #!/usr/bin/env bash
-# Run rev2 on all the data (with/without fake data)
+# Generate fake data
 
 # The name of the job, can be anything, simply used when displaying the list of running jobs
-#$ -N fake-test
+#$ -N fake-$data-$k-$n
 # Combining output/error messages into one file
 #$ -j y
 # Set memory request:
 #$ -l vf=2G
-#$ -pe smp 10
 # Set walltime request:
-#$ -l h_rt=10:59:59
+#$ -l h_rt=0:29:59
 # One needs to tell the queue system to use the current directory as the working directory
 # Or else the script may fail as it will execute in your top level home directory /home/username
 #$ -cwd
@@ -18,19 +20,16 @@
 # Now comes the command to be executed
 source $HOME/venv/bin/activate
 
-data=$1
-
 # for data in alpha amazon epinions otc; do
-for k in $(seq 0 10); do
-    for n in $(seq 0 10); do
-        echo "$data, $k, $n"
-        OUTPUT_DIR="../rev2res/fake-$data-$k-$n/"
-        if [ ! -d $OUTPUT_DIR ]; then
-            mkdir $OUTPUT_DIR
-        fi
-        bash run-rev2-all-params.sh $data $k $n 100
-    done
-done
-wait
+python ./fake.py $data $k $n
 # done
+
 exit 0
+'''
+
+for d in ['alpla', 'amazon', 'epinions', 'otc']:
+    for k in range(11):
+        for n in range(11):
+            tmp = sample.replace('$k', str(k)).replace('$n', str(n)).replace('$data', d)
+            with open('fake-%s-%d-%d.qjob' %(d, k, n), 'w') as f:
+                f.write(tmp)
