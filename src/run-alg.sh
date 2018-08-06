b6b450b81e36294eb930e260f75748481a8813ed
Rui Liu <rliu@cs.dartmouth.edu>
2018-08-06 18:34:40 -0400

    run-alg get multiple args

--- src/run-alg.sh
+++ src/run-alg.sh
@@ -1,28 +1,37 @@
 #!/usr/bin/env bash
 
-if [ $# -ne 2 ]; then
-    echo "usage: run-alg.sh alg data"
+if [ $# -le 1 ]; then
+    echo "usage: run-alg.sh alg [data+]"
     exit 1
 fi
 
 alg=$1
-data=$2
 
-OUTPUT_DIR="../${alg}res/$data/"
-if [ ! -d $OUTPUT_DIR ]; then
-    mkdir -p $OUTPUT_DIR
-    echo "create $OUTPUT_DIR"
-fi
+function run() {
+    data=$1
+    echo "run $ alg $data"
+    OUTPUT_DIR="../${alg}res/$data/"
+    if [ ! -d $OUTPUT_DIR ]; then
+        mkdir -p $OUTPUT_DIR
+        echo "create $OUTPUT_DIR"
+    fi
 
-for k in $(seq 0 10)
-do
-    for N in $(seq 0 10)
+    for k in $(seq 0 10)
     do
-        for ind in $(seq 0 19)
+        for N in $(seq 0 10)
         do
-            echo ${alg}run.py $data $k $N $ind
-            python ${alg}run.py $data $k $N $ind &
+            for ind in $(seq 0 19)
+            do
+                echo ${alg}run.py $data $k $N $ind
+                python ${alg}run.py $data $k $N $ind &
+            done
+            wait;
         done
-        wait;
     done
+}
+
+shift
+for var in "$@"
+do
+    run $var
 done
