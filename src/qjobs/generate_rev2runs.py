91b9ea7e66767cd457d487668a0bb51ff8c7d8aa
Rui Liu <rliu@cs.dartmouth.edu>
2018-08-24 13:33:04 -0400

    args for qjobs

--- src/qjobs/generate_rev2runs.py
+++ src/qjobs/generate_rev2runs.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 
+import sys, os
+import argparse
+
 sample='''
 #!/usr/bin/env bash
 # Run rev2 on all the data (with/without fake data)
@@ -10,9 +13,10 @@ sample='''
 #$ -j y
 # Set memory request:
 #$ -l vf=1G
+#$ -l ironfs
 #$ -pe smp 8
 # Set walltime request:
-#$ -l h_rt=2:59:59
+#$ -l h_rt=3:59:59
 # One needs to tell the queue system to use the current directory as the working directory
 # Or else the script may fail as it will execute in your top level home directory /home/username
 #$ -cwd
@@ -34,12 +38,22 @@ wait
 exit 0
 '''
 
-step = 10
-
-for d in ['alpha', 'amazon', 'epinions', 'otc']:
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Exploits pages')
+    parser.add_argument('-d', '--data', action='store', choices=['alpha', 'amazon', 'epinions', 'otc'], required=True, help='target dataset')
+    parser.add_argument('-i', '--index', action='store', nargs=2, required=True, help='start and end index of target product')
+    parsed = parser.parse_args(sys.argv[1:])
+    
+    print(parsed)
+    
+    step = 10
+    d = parsed.data
+    i_s = int(parsed.index[0])
+    i_e = int(parsed.index[1])
+    
     for k in range(11):
         for n in range(11):
-            for inds in range(0, 100, step):
+            for inds in range(i_s, i_e, step):
                 qjob_name = 'rev2run-%s-%d-%d-%d.qjob' %(d, k, n, inds)
                 tmp = sample.replace('$k', str(k)).replace('$n', str(n)).replace('$data', d).replace('$inds', str(inds)).replace('$inde', str(inds + step - 1)).replace('$qjob_name', qjob_name)
                 with open(qjob_name, 'w') as f:
