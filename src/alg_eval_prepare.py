ad3e57275a84bdaf3f14bbbdc153ff8f36d70c4e
Rui Liu <rliu@cs.dartmouth.edu>
2018-09-04 01:37:57 -0400

    alpha milestone

--- src/alg_eval_prepare.py
+++ src/alg_eval_prepare.py
@@ -3,6 +3,7 @@
 import os, sys, argparse
 import numpy as np
 import pandas as pd
+import numba
 
 import sklearn
 
@@ -14,10 +15,12 @@ import pickle
 parser = argparse.ArgumentParser(description='convert csv to pickle')
 parser.add_argument('-d', '--data', type=str, default='alpha', help='data name')
 parser.add_argument('-a', '--alg', type=str, default='bad', help='alg name')
+parser.add_argument('-n', '--ncores', type=int, default=-1, help='number of cores to use')
 parsed = parser.parse_args(sys.argv[1:])
 
 data_name = parsed.data
 alg_name = parsed.alg
+n_cores = parsed.ncores
 print(alg_name, data_name)
 
 network_df = pd.read_csv('../rev2data/%s/%s_network.csv' %(data_name, data_name), header=None, names=['src', 'dest', 'rating', 'timestamp'], parse_dates=[3], infer_datetime_format=True)
@@ -39,11 +42,12 @@ def parse_data(df):
         sortedret = sorted(ret, key=lambda x: x[2])
     return pd.DataFrame(sortedret)
 
+@numba.jit
 def compute_score(k=0, n=0, ind=0):
     if alg_name == 'rev2':
-        results_df = pd.read_csv('../%sres/%s/%s-1-1-1-1-1-1-0-%d-%d-%d.csv' %(alg_name, data_name, data_name, k, n, ind), header=None)
+        results_df = pd.read_csv('../res/%s/%s/%s-1-1-1-1-1-1-0-%d-%d-%d.csv' %(alg_name, data_name, data_name, k, n, ind), header=None)
     else:
-        results_df = pd.read_csv('../%sres/%s/%s-%d-%d-%d.csv' %(alg_name, data_name, data_name, k, n, ind), header=None)
+        results_df = pd.read_csv('../res/%s/%s/%s-%d-%d-%d.csv' %(alg_name, data_name, data_name, k, n, ind), header=None)
     if len(results_df.columns) < 3: results_df = parse_data(results_df)
     # if user is good in ground truth output 0
     # if user is fraudster in ground truth output 1
@@ -53,9 +57,9 @@ def compute_score(k=0, n=0, ind=0):
     yscore = results_df[2].tolist()
     return {'ulist': ulist, 'ytrue': ytrue, 'yscore': yscore}
 
-results = Parallel(n_jobs=-1, verbose=3)(delayed(compute_score)(k, n, ind) for k, n, ind in itertools.product(range(11), range(11), range(50)))
+results = Parallel(n_jobs=n_cores, verbose=3)(delayed(compute_score)(k, n, ind) for k, n, ind in itertools.product(range(11), range(11), range(50)))
 
 results_dict = dict(zip(itertools.product(range(11), range(11), range(50)), results))
 
-with open('../%sres/%s.pkl' %(alg_name, data_name), 'wb') as f:
+with open('../res/%s/%s.pkl' %(alg_name, data_name), 'wb') as f:
     pickle.dump(results_dict, f)
