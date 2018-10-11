25872029dd58301d964e1031a4fc10eb4d715ae6
Rui Liu <rliu@cs.dartmouth.edu>
2018-10-11 14:47:39 -0400

    mkdir for each alg

--- src/qjobs/create_benchmarks.py
+++ src/qjobs/create_benchmarks.py
@@ -7,7 +7,7 @@ template = '''
 #!/usr/bin/env bash
 
 # The name of the job, can be anything, simply used when displaying the list of running jobs
-#$ -N $algorithm-$k-$n-$data
+#$ -N $algorithm-$data-$k-$n-$ind
 # Combining output/error messages into one file
 #$ -j y
 
@@ -21,7 +21,7 @@ template = '''
 ### -pe smp 8
 
 # Set walltime request:
-#$ -l h_rt=$time
+#$ -l h_rt=0:59:59
 
 # One needs to tell the queue system to use the current directory as the working directory
 # Or else the script may fail as it will execute in your top level home directory /home/username
@@ -48,7 +48,7 @@ exit 0
 '''
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Exploits pages')
+    parser = argparse.ArgumentParser(description='create qjobs')
     parser.add_argument('-d', '--data', action='store', choices=['alpha', 'amazon', 'epinions', 'otc'], default='alpha', help='target dataset')
     parser.add_argument('-c', '--clean', action='store_true', help='clean up the qjobs and outputs')
     parser.add_argument('-a', '--alg', action='store', choices=['bn', 'feagle', 'fraudar', 'trust', 'rsd', 'bad'], default='bn', help='algorithm')
@@ -72,7 +72,13 @@ if __name__ == '__main__':
     n_range = list(range(0, 51, 5))
     n_range[0] = 1
 
+    if not os.path.exists(parsed.alg):
+        os.mkdir(parsed.alg)
+
     for k in range(10):
         for n in n_range:
             for ind in range(50):
-                script = template.replace('$data', parsed.data).replace('$k', str(k)).replace('$N', str(n)).replace('$algorithm', parsed.alg).replace('$ind', str(ind)).replace('$time', '0:59:59')
+                qjob_name = '%s-%s-%d-%d-%d.qjob' %(parsed.alg, parsed.data, k, n, ind)
+                script = template.replace('$data', parsed.data).replace('$k', str(k)).replace('$N', str(n)).replace('$algorithm', parsed.alg).replace('$ind', str(ind))
+                with open(os.path.join(parsed.alg, qjob_name), 'w') as fp:
+                    fp.write(script)
