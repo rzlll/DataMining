73ad5f37d49c7400782c6335a47b38b057d61686
Rui Liu <rliu@cs.dartmouth.edu>
2018-11-19 16:08:29 -0500

    log dir

--- src/qjobs/create_benchmarks.py
+++ src/qjobs/create_benchmarks.py
@@ -28,7 +28,7 @@ template = '''
 
 # the cwd is somehow causing problems
 ####$ -cwd
-#$ -wd /home/ifsdata/scratch/rliu/qlog/
+#$ -wd $HOME/research/fake-review/src/qjobs/$algorithm/
 
 # then you tell it retain all environment variables (as the default is to scrub your environment)
 ####$ -V
