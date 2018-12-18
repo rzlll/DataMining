40b8e1f76b3562db6cec356de4ab3a368e490a30
Rui Liu <rliu@cs.dartmouth.edu>
2018-12-18 17:38:35 -0500

    less iters

--- src/algs/feagle.py
+++ src/algs/feagle.py
@@ -29,16 +29,16 @@ products = {n: graph.new_product(n) for n in G.node if n.startswith('p')}
 for e in G.edges:
     graph.add_review(reviewers[e[0]], products[e[1]], G.edges[e]['weight'])
 
-for it in range(5):
+for it in range(3):
     diff = graph.update()
     print('iter %d, diff %.2f' %(it, diff))
-    if diff < 1e-3:
+
+    feagle_list = [[out_dict[r.name], r.name, r.anomalous_score] for r in graph.reviewers if r.name in out_dict]
+    out_list = [[x[0]] + ['s' + x[1][1:]] + x[2:] if x[1] in socks_list else x for x in feagle_list]
+    # fraud eagle output anomaly score instead of goodness score
+    out_list = sorted(out_list, key=lambda x: -x[2])
+    pd.DataFrame(out_list).to_csv(outfile, header=False, index=False)
+
+    if diff < 1e-2:
         print('early stop')
         break
-
-feagle_list = [[out_dict[r.name], r.name, r.anomalous_score] for r in graph.reviewers if r.name in out_dict]
-
-out_list = [[x[0]] + ['s' + x[1][1:]] + x[2:] if x[1] in socks_list else x for x in feagle_list]
-# fraud eagle output anomaly score instead of goodness score
-out_list = sorted(out_list, key=lambda x: -x[2])
-pd.DataFrame(out_list).to_csv(outfile, header=False, index=False)
--- src/algs/trust.py
+++ src/algs/trust.py
@@ -58,7 +58,7 @@ dr = 0
 dt = 0
 dh = 0
     
-while iter_idx < 5:
+while iter_idx < 3:
     print("-----\niter: ", iter_idx)
     
     print(dr, dt, dh)
